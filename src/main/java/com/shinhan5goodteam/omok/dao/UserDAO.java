2f7c7d73e4f6324f28a1e575967a1e372a72e211
eunhyo <eun261126@naver.com>
2025-05-21 18:49:17 +0900

    [eunhyo] feat: 로그인 기능 구현 DOC: 로그인 jsp 변경경

--- src/main/java/com/shinhan5goodteam/omok/dao/UserDAO.java
+++ src/main/java/com/shinhan5goodteam/omok/dao/UserDAO.java
@@ -2,23 +2,49 @@ package com.shinhan5goodteam.omok.dao;
 
 import java.sql.*;
 
+import com.shinhan5goodteam.omok.model.User;
+
 public class UserDAO {
 
-    public boolean isUserIdExist(String userid){
+    public boolean isUserIdExist(String userid) {
         String sql = "select * from USER where user_id = ?";
-        try( Connection conn = DButil.getConnection();
-            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        try (Connection conn = DButil.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-                pstmt.setString(1, userid);
-                ResultSet rs = pstmt.executeQuery();
+            pstmt.setString(1, userid);
+            ResultSet rs = pstmt.executeQuery();
 
-                if (rs.next()){
-                    return true;
-                }
-        } catch ( Exception e) {
+            if (rs.next()) {
+                return true;
+            }
+        } catch (Exception e) {
 
         }
 
         return false;
+    }
+
+    // 로그인용 메서드 추가
+    public User login(String userid, String userpw) {
+        String sql = "SELECT * FROM USER_TABLE WHERE user_id = ? AND user_pw = ?";
+        try (Connection conn = DButil.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, userid);
+            pstmt.setString(2, userpw);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                User user = new User();
+                user.setUserid(rs.getString("user_id"));
+                user.setUserpw(rs.getString("user_pw"));
+                user.setNickname(rs.getString("nickname")); 
+
+                return user;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
